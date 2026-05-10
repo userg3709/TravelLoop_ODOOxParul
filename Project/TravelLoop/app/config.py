0118abbc333059f24515e8d2551a2f147e8d8eeb
ROHAN MACHA <98206744+userg3709@users.noreply.github.com>
2026-05-10 11:13:05 +0530

    Delete Project directory

--- Project/TravelLoop/app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
-    DATABASE = os.path.join(os.path.dirname(os.path.dirname(__file__)), "instance", "travelloop.sqlite")
